fc4e9f2d16c42fe9a331873caf5e0586bd45f605
Damian <xuamp@yahoo.com.ar>
2023-05-21 21:17:03 -0300

    Conecta la app con la base de datos de Firebase

---
+++ src/Componentes/FirebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import {  collection, getFirestore  }from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD081gYvSha3vdzC7FzkzkvjWWd81q9gcU",
+  authDomain: "base02-dami.firebaseapp.com",
+  projectId: "base02-dami",
+  storageBucket: "base02-dami.appspot.com",
+  messagingSenderId: "1073248744846",
+  appId: "1:1073248744846:web:1437c99708d6f46f94a0df"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+export const productCollection = collection(db, "productos");
